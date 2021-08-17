c5f371af844c62a5fc5337517d47e9ac02afa330
oldnavy281 <oldnavy281@gmail.com>
2021-08-17 08:32:39 -0600

    commit

--- routes/routes.js
+++ routes/routes.js
@@ -1,3 +1,15 @@
 const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
+
+mongoose.connect('mongodb+srv://admin:<AbC1234!>@cluster0.mo9pt.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
+    useUnifiedTopology: true,
+    useNewUrlParser: true
+});
+
+mongoose.set('useCreateIndex', true);
+mongoose.set('useFindAndModify', true);
+
+let mdb = mongoose.connection;
+mdb.on('error', console.error.bind(console, 'connection error'));
+mdb.once('open', callback => {});
