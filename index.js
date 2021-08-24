0e6b7f5378f46e9f2bdf7f93804dd22884c46f2c
rbenner7 <50886969+rbenner7@users.noreply.github.com>
2021-08-23 21:58:50 -0600

    jjj

--- index.js
+++ index.js
@@ -59,6 +59,12 @@ app.get((req, res) => {
     };
 });
 
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
 app.get('/', routes.index);
 app.get('/landing', routes.landing);
 app.get('/login', routes.login);
